ecfd94b771543ec75e21fac5dd161ef51f5b6774
RicardoVallejos92 <richard_22_100@hotmail.com>
2023-01-20 01:42:09 -0300

    ML-(organizado y animando elementos)

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(port,() => console.log('servidor corriendo en http://localhost:'+ port));
+
+app.get('/',(req, res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/login',(req, res) => res.sendFile(path.join(__dirname,'views','login.html')));
+app.get('/register',(req, res) => res.sendFile(path.join(__dirname,'views','register.html')));
